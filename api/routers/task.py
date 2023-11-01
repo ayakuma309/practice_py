be96a3750cf020ae039e92eac5e560f047ee2b5d
ayakuma309 <ayako.30901@gmail.com>
2023-11-01 09:05:32 +0900

    add: post関数

--- api/routers/task.py
+++ api/routers/task.py
@@ -9,9 +9,13 @@ async def list_tasks():
     return [task_schema.Task(id=1, title="1つ目のTODOタスク")]
 
 
-@router.post("/tasks")
-async def create_task():
-    pass
+@router.post("/tasks", response_model=task_schema.TaskCreateResponse)
+#引数にリクエストボディ
+async def create_task(task_body: task_schema.TaskCreate):
+# dict に変換し、これらのkey/valueおよび id=1 を持つ task_schema.TaskCreateResponse インスタンスを作成
+    return task_schema.TaskCreateResponse(id=1, **task_body.dict())
+
+
 
 
 @router.put("/tasks/{task_id}")
--- api/schemas/task.py
+++ api/schemas/task.py
@@ -2,10 +2,30 @@ from typing import Optional
 
 from pydantic import BaseModel, Field
 
+# 共通部分
+class TaskBase(BaseModel):
+    title: Optional[str] = Field(None, example="クリーニングを取りに行く")
+
+# passにする= 何もしない文
+class TaskCreate(TaskBase):
+    pass
+
+
+class TaskCreateResponse(TaskCreate):
+    id: int
+
+    class Config:
+        orm_mode = True
+
+# 共通部分titleを除く
 # id, title, done の３フィールド
 # それぞれint, Optional[str], bool の型ヒント
 # Field はフィールドに関する付加情報
-class Task(BaseModel):
+class Task(TaskBase):
     id: int
-    title: Optional[str] = Field(None, example="クリーニングを取りに行く")
     done: bool = Field(False, description="完了フラグ")
+
+# orm_mode はDBと接続する際に使用
+# レスポンススキーマ TaskCreateResponse が、暗黙的にORMを受け取り、レスポンススキーマに変換することを意味
+    class Config:
+        orm_mode = True
